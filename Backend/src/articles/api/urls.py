3d5a9fccd5779fd54aae84619a3d809e60873614
Husnain Ahmad <husnainahmad521@gmail.com>
2020-08-19 21:48:27 +0500

    second

--- Backend/src/articles/api/urls.py
+++ Backend/src/articles/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ArticleViewSet 
 from .views import ArticleDetailsSet
+from .views import ArticleCreateSet
 
 urlpatterns = [
     path('', ArticleViewSet.as_view()),
+    path('create/',ArticleCreateSet.as_view()),
     path('<pk>', ArticleDetailsSet.as_view())
     ]
--- Backend/src/articles/api/views.py
+++ Backend/src/articles/api/views.py
@@ -2,6 +2,7 @@ from .serializers import ArticleSerializer
 from articles.models import Article
 from rest_framework.generics import ListAPIView 
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import CreateAPIView
 class ArticleViewSet(ListAPIView):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
@@ -9,3 +10,7 @@ class ArticleViewSet(ListAPIView):
 class ArticleDetailsSet(RetrieveAPIView):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+
+class ArticleCreateSet(CreateAPIView):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()    
